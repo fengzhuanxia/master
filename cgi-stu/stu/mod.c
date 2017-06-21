ee13395dbe477a8acc38bab36d4c514018028921
Richard.Wang <wangleihd@gmail.com>
2017-06-21 14:36:07 +0800

    new file
    new file

---
+++ cgi-stu/stu/mod.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <mysql/mysql.h>
+#include "cgic.h"
+
+int cgiMain()
+{
+
+	fprintf(cgiOut, "Content-type:text/html;charset=utf-8\n\n");
+
+	char id[32] = "\0";
+	char sname [16] = "\0";
+	char age[32] = "\0";
+	char sex[32]="\0";
+	char dept[32]="\0";
+	int status = 0;
+
+	status = cgiFormString("id",  id, 32);
+	if (status != cgiFormSuccess)
+	{
+		fprintf(cgiOut, "get id error!\n");
+		return 1;
+	}
+
+	status = cgiFormString("sname",  sname, 16);
+	if (status != cgiFormSuccess)
+	{
+		fprintf(cgiOut, "get sname error!\n");
+		return 1;
+	}
+
+	status = cgiFormString("age", age, 32);
+	if (status != cgiFormSuccess)
+	{
+		fprintf(cgiOut, "get age error!\n");
+		return 1;
+	}
+	status = cgiFormString("sex",  sex, 32);
+	if (status != cgiFormSuccess)
+	{
+		fprintf(cgiOut, "get sex error!\n");
+		return 1;
+	}
+
+	status = cgiFormString("dept",  dept, 32);
+	if (status != cgiFormSuccess)
+	{
+		fprintf(cgiOut, "get dept error!\n");
+		return 1;
+	}
+
+	//fprintf(cgiOut, "name = %s, age = %s, stuId = %s\n", name, age, stuId);
+
+	int ret;
+	char sql[128] = "\0";
+	MYSQL *db;
+
+	//初始化
+	db = mysql_init(NULL);
+	if (db == NULL)
+	{
+		fprintf(cgiOut,"mysql_init fail:%s\n", mysql_error(db));
+		return -1;
+	}
+
+	//连接数据库
+	db = mysql_real_connect(db, "127.0.0.1", "root", "123456", "stu",  3306, NULL, 0);
+	if (db == NULL)
+	{
+		fprintf(cgiOut,"mysql_real_connect fail:%s\n", mysql_error(db));
+		mysql_close(db);
+		return -1;
+	}
+
+
+	sprintf(sql, "update student set id= %d, sname='%s', age= %d, sex='%s', dept='%s' where id = %d ",
+																					atoi(id),sname,atoi(age),sex,dept );
+	if ((ret = mysql_real_query(db, sql, strlen(sql) + 1)) != 0)
+	{
+		fprintf(cgiOut,"mysql_real_query fail:%s\n", mysql_error(db));
+		mysql_close(db);
+		return -1;
+	}
+
+
+
+	fprintf(cgiOut, "update student ok!\n");
+	mysql_close(db);
+	return 0;
+}
